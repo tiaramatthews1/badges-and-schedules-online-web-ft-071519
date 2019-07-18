77aaa5fe32dd4fdbc91228fb6e7eef10cafd4448
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-18 15:36:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do 
+  batch_badge_creator(attendees).each do |badge|
  puts badge 
 end
 assign_rooms(attendees).each do |assignment|
