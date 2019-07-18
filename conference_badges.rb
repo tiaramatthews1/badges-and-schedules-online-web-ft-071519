1828385ed7b31a65aab238f8de5faf454ed1c122
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-18 15:33:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 
-def badge_maker(names)
- return "Hello, my name is #{names}."
+def badge_maker(attendees)
+ return "Hello, my name is #{attendees}."
 end
 
 def batch_badge_creator(names)
