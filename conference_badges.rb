ccdbd8e3dd84ffbc1da343359f452d62ba1fdcdc
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-18 15:24:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees_lineup)
-  room_assignments.each_with_index.map {|attendees, index|} "Hello #{attendees}! You'll be assigned to room #{index +1}!"
+  attendees_lineup.each_with_index.map {|attendees, index|} "Hello #{attendees}! You'll be assigned to room #{index +1}!"
   puts "Hello, #{attendees}! you'll be assigned to room #{room_assignments}"
 end
 
